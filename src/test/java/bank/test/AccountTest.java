5824077423cdfd8902f154f92b0e57b16a0ce733
Hcen Tlili <htlili@vermeg.com>
2023-07-09 23:56:41 +0100

    US1: Deposit

--- src/test/java/bank/test/AccountTest.java
+++ src/test/java/bank/test/AccountTest.java
@@ -21,5 +21,22 @@ class AccountTest {
         account = new Account();
     }
 
+  @Test
+    void depositPositiveAmount() {
+        account.deposit(100.0);
+        assertEquals(100.0, account.getBalance());
+    }
+
+    @Test
+    void depositInvlalidAmount() {
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(0.0));
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(-50.0));
+    }
+
+    @Test
+    void depositNUll() {
+        assertThrows(IllegalArgumentException.class, () -> account.deposit((Double) null));
+    }
+
    
 }
